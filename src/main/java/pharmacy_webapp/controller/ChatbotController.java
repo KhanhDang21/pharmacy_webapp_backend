d0df4481d8d5cd0d35df7a6370e56fea5b000c79
KhanhDang21 <qxdangvietanh@gmail.com>
2026-03-11 07:56:46 +0700

    Updated chatbot v2

--- src/main/java/pharmacy_webapp/controller/ChatbotController.java
+++ src/main/java/pharmacy_webapp/controller/ChatbotController.java
@@ -3,6 +3,7 @@ package pharmacy_webapp.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pharmacy_webapp.dto.ApiResponse;
 import pharmacy_webapp.service.ChatbotService;
 
 import java.util.Map;
@@ -15,23 +16,31 @@ public class ChatbotController {
     private final ChatbotService chatbotService;
 
     @PostMapping
-    public ResponseEntity<?> chat(@RequestHeader(value = "X-Session-Id", defaultValue = "default") String sessionId,
-                                  @RequestParam String message) {
+    public ResponseEntity<ApiResponse<String>> chat(@RequestHeader(value = "X-Session-Id", defaultValue = "default") String sessionId,
+                                            @RequestParam String message) {
         try {
             if (message == null || message.isBlank()) {
-                return ResponseEntity.badRequest().body(Map.of("error", "Message not null"));
+                return ResponseEntity.badRequest().body(
+                        ApiResponse.error("Something went wrong")
+                );
             }
             String reply = chatbotService.chat(sessionId, message);
-            return ResponseEntity.ok(Map.of("reply", reply));
+            return ResponseEntity.ok(
+                    ApiResponse.success(reply, "Chatbot successfully reply")
+            );
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(Map.of("error", e.getMessage()));
+            return ResponseEntity.badRequest().body(
+                    ApiResponse.error(e.getMessage())
+            );
         }
     }
 
     @DeleteMapping("/history")
-    public ResponseEntity<?> clearHistory(
+    public ResponseEntity<ApiResponse<String>> clearHistory(
             @RequestHeader(value = "X-Session-Id", defaultValue = "default") String sessionId) {
         chatbotService.clearHistory(sessionId);
-        return ResponseEntity.ok(Map.of("message", "Deleted history message"));
+        return ResponseEntity.ok(
+                ApiResponse.success("message", "Deleted history message")
+        );
     }
 }
